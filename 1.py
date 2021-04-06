2b3bf150feb5a92dbb952348a676a860ad55a206
yusrilarzaqi <70110733+yusrilarzaqi@users.noreply.github.com>
2021-04-06 23:15:33 +0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Persiapan sample data
+x = [1, 2, 3, 4]
+y=[1,4,2,3]
+fig,ax=plt.subplots()
+ax.plot(x,y)
+plt.show()
+
---
+++ 2.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([1, 2, 3, 4])
+y = np.array([1, 4, 2, 3])
+
+plt.plot(x, y)
+plt.show()
+
