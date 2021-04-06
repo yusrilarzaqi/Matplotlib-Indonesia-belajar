bd7ae8b861fb9369214a2b3c90f6ea50d0912c35
yusrilarzaqi <70110733+yusrilarzaqi@users.noreply.github.com>
2021-04-06 23:09:57 +0700

    Tutorial
    I donno

---
+++ Git.bash
@@ -0,0 +1,7 @@
+echo "# Matplotlib-Indonesia-belajar-" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yusrilarzaqi/Matplotlib-Indonesia-belajar-.git
+git push -u origin main
